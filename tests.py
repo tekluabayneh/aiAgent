2545a522c5d51e1a472345bcb3e18800e638e880
tekluabayneh <tekluabayneh@gmail.com>
2025-09-13 19:02:30 +0000

    test: test get_file_content function

--- tests.py
+++ tests.py
@@ -1,15 +1,19 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file 
 
 def main(): 
     working_dir = "calculator" 
-    root_contents = get_files_info(working_dir) 
-    pkg_content = get_files_info(working_dir, "pkg")
-    pkg_content = get_files_info(working_dir, "/bin")
-    pkg_content = get_files_info(working_dir, "../")
-    print(root_contents)
-    print(pkg_content)
+    # root_contents = get_files_info(working_dir) 
+    # pkg_content = get_files_info(working_dir, "pkg")
+    # pkg_content = get_files_info(working_dir, "/bin")
+    # pkg_content = get_files_info(working_dir, "../")
+    # print(root_contents)
+    # print(pkg_content)
+    #
 
-
+    # print(get_file_content(working_dir, "lorem.txt"))
+    print(write_file(working_dir, "man.txt", "this is the file man"))
 
 
 main()
