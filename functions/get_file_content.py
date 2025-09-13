0f952aae4efb0583a2a6bc9e45eebb2df605d19f
tekluabayneh <tekluabayneh@gmail.com>
2025-09-13 19:04:11 +0000

    refactore: added edge case checking file apth and fix absolute path error

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,19 +1,32 @@
+import os
 MAX_CHARS = 1000
 
 def get_file_content(working_directory, file_path): 
     abs_working_dir = os.path.abspath(working_directory)
-    abs_file_path = os.path.abspath(working_dir)
-    abs_direcotry  = os.path.abspath(os.path.join(working_directory, file_path))
-
+    abs_file_path = os.path.abspath(file_path)
+    abs_direcotry  = os.path.abspath(os.path.join(working_directory,  file_path))
+ 
     if not abs_direcotry.startswith(abs_working_dir): 
         return f"Errors: {abs_file_path} is not a directory"
 
-    if not os.path.isfile(abs_file_path): 
+    if not os.path.isfile(abs_direcotry): 
         return f"Errors: {file_path} is not a file"
 
-    get_file_content = "" 
-    with open(abs_file_path, "r") as f: 
-        get_file_content = f.read(MAX_CHARS)
+
+    file_content_setting = "" 
+
+    try:
+        with open(abs_direcotry, "r") as f: 
+            file_content_setting = f.read(MAX_CHARS)
+
+            if len(file_content_setting) >= MAX_CHARS: 
+                file_content_setting += f"...file {file_path}, truncated at 10000 char" 
+
+    except Exception as e:
+        return f"Exeption reading file: {e}"
+
+
+    return file_content_setting
 
 
 
